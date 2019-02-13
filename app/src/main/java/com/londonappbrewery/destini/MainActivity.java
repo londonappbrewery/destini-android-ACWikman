b0408867e2a6a0f0c30b3508ddbeeaeafe1a0c23
ACWikman <alexander.wikman@gmail.com>
2019-02-13 20:21:27 +0100

    Did I pass...? :)

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,18 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    TextView mTextView;
+    Button mButtonTop;
+    Button mButtonBottom;
+    int mStoryIndex = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +22,80 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mTextView = findViewById(R.id.storyTextView);
+        mButtonTop = findViewById(R.id.buttonTop);
+        mButtonBottom = findViewById(R.id.buttonBottom);
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+        mButtonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mStoryIndex == 1)
+                {
+                    mStoryIndex = 3;
+                } else if (mStoryIndex == 2)
+                {
+                    mStoryIndex = 3;
+                } else if (mStoryIndex == 3)
+                {
+                    mStoryIndex = 6;
+                }
 
+                UpdateText();
+
+            }
+        });
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        mButtonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mStoryIndex == 1)
+                {
+                    mStoryIndex = 2;
+                } else if (mStoryIndex == 2)
+                {
+                    mStoryIndex = 4;
+                } else if (mStoryIndex == 3) {
+                    mStoryIndex = 5;
+                }
 
+                UpdateText();
 
+            }
+        });
+
+    }
+    public void UpdateText ()
+    {
+        if(mStoryIndex == 4 || mStoryIndex == 5 || mStoryIndex == 6)
+        {
+            mButtonTop.setVisibility(View.GONE);
+            mButtonBottom.setVisibility(View.GONE);
+
+            String textID = "T" + mStoryIndex + "_End"; //"R.string.T" + mStoryIndex + "_Story";
+            int resID = getApplicationContext().getResources().getIdentifier(textID, "string", "com.londonappbrewery.destini");
+            mTextView.setText(getString(resID));
+
+        } else {
+            // Update story
+            String textID = "T" + mStoryIndex + "_Story"; //"R.string.T" + mStoryIndex + "_Story";
+            int resID = getApplicationContext().getResources().getIdentifier(textID, "string", "com.londonappbrewery.destini");
+            mTextView.setText(getString(resID));
+
+            // Update top button
+            textID = "T" + mStoryIndex + "_Ans1"; //"R.string.T" + mStoryIndex + "_Story";
+            resID = getApplicationContext().getResources().getIdentifier(textID, "string", "com.londonappbrewery.destini");
+            mButtonTop.setText(getString(resID));
+
+            // Update bottom button
+            textID = "T" + mStoryIndex + "_Ans2"; //"R.string.T" + mStoryIndex + "_Story";
+            resID = getApplicationContext().getResources().getIdentifier(textID, "string", "com.londonappbrewery.destini");
+            mButtonBottom.setText(getString(resID));
+        }
     }
 }
